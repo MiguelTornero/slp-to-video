54915987aae99f7ce3b0fd8b7c6fb2dcc5c6136b
Miguel Tornero <miguel.tornero.carrillo@gmail.com>
2024-03-21 10:02:35 -0600

    added test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,17 +1,18 @@
 import { ok } from "node:assert"
-
 import { spawnSync } from "node:child_process"
 
 describe("run cmd", () => {
 
     it("should show help on stderr", () => {
         const cmd = spawnSync("./bin/dev.ts")
+        ok(cmd.status !== 0)
         ok(!cmd.stdout.toString())
         ok(!!cmd.stderr.toString())
     })
     
     it("should show help on stdout", () => {
         const cmd = spawnSync("./bin/dev.ts", ["-h"])
+        ok(cmd.status === 0)
         ok(!!cmd.stdout.toString())
         ok(!cmd.stderr.toString())
     })
